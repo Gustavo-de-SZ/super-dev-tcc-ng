d9e0c149cfdebc3d4c434a1032c739cdf426f78e
Gustavo de Souza <072069@PROWAY.TREINA>
2025-10-20 21:36:58 -0300

    aula

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'userlogin',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class UserLogin {
+
+}
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Documentation } from './documentation/documentation';
 import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
+import { Home } from '@/pages/home/home';
+import { Perfil } from './perfil/perfil';
 
 export default [
     { path: 'documentation', component: Documentation },
     { path: 'crud', component: Crud },
     { path: 'empty', component: Empty },
-    { path: '**', redirectTo: '/notfound' }
+    { path: '**', redirectTo: '/notfound' },
+    { path: 'perfil', component: Perfil},
+   
 ] as Routes;
